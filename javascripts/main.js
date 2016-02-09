c3d54977d302b8692982a38f062403f5fc432c16
Tomás Fox <tomas.c.fox@gmail.com>
2016-02-08 21:17:04 -0300

    Create gh-pages branch via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,1 @@
-angular.module('app', ['katex'])
-    .controller('Ctrl', Ctrl);
-
-Ctrl.$inject = ['$scope'];
-
-function Ctrl($scope) {
-  'use strict';
-
-  $scope.tex = 'x^2';
-}
+console.log('This would be the main JS file.');
