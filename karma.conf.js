ee50de4bf0a894ee30d7ef44887f2e65d5f86003
Tomás Fox <tomas.c.fox@gmail.com>
2016-03-09 21:39:59 -0300

    Change dependencies version to a fixed one

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/katex/dist/katex.min.js',
